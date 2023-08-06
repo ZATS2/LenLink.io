e1cf0c21507acaadd5c1b3f7d21e4a20bac49a3e
ZATS2 <96290968+ZATS2@users.noreply.github.com>
2023-08-07 03:23:48 +0700

    Update save_data.php

--- save_data.php
+++ save_data.php
@@ -1,9 +1,1 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $userInput = $_POST["user_input"];
-    $file = fopen("user_input.txt", "w");
-    fwrite($file, $userInput);
-    fclose($file);
-    echo "Data saved successfully!";
-}
-?>
+
